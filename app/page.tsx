9ccd045485304a836c17e75707da5616f0603787
d8uv <d8uv@d8uv.org>
2023-07-29 18:11:43 -0500

    prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,27 @@
 "use client";
 
-import {
-  JSXElementConstructor,
-  PromiseLikeOfReactNode,
-  ReactElement,
-  ReactNode,
-  ReactPortal,
-  useState,
-} from "react";
+import { useState } from "react";
 
 function Header() {
   return (
     <header className="text-center">
       <h1 className="">
-        <span className="block text-3xl sm:text-4xl font-semibold leading-none">
+        <span className="block text-3xl font-semibold leading-none sm:text-4xl">
           <span
             role="none"
-            className="p-2 font-symbols font-normal select-none"
+            className="select-none p-2 font-symbols font-normal"
           >
             🙠
           </span>
           Dice to Distribution
           <span
             role="none"
-            className="p-2 font-symbols  font-normal select-none"
+            className="select-none p-2 font-symbols font-normal"
           >
             🙢
           </span>
         </span>
-        <span className="block text-2xl sm:text-3xl italic leading-none">
+        <span className="block text-2xl italic leading-none sm:text-3xl">
           and the Killable Zone
         </span>
       </h1>
@@ -43,10 +36,15 @@ function Calculator(props: {
 }) {
   return (
     <div className="my-8 text-center">
-      <div className="border-2 border-gray-300 dark:border-gray-700 rounded-xl shadow-lg overflow-hidden">
-        <div className="bg-gray-200 dark:bg-gray-800 p-4 border-b-2 border-gray-300 dark:border-gray-700">
+      <div className="overflow-hidden rounded-xl border-2 border-gray-300 shadow-lg dark:border-gray-700">
+        <div className="border-b-2 border-gray-300 bg-gray-200 p-4 dark:border-gray-700 dark:bg-gray-800">
           <form>
-            <p><label>number of dice <span className="italic">(A)</span>: <input type="number"/></label></p>
+            <p>
+              <label>
+                number of dice <span className="italic">(A)</span>:{" "}
+                <input type="number" />
+              </label>
+            </p>
           </form>
         </div>
         <div className="p-4">
@@ -63,8 +61,8 @@ export default function Home() {
   const [rollModifier, setRollModifier] = useState<number>(0);
 
   return (
-    <main className="min-h-screen py-16 px-4 bg-gray-50 text-grey-950 dark:bg-gray-950 dark:text-gray-50 text-base sm:text-lg">
-      <div className="max-w-2xl mx-auto">
+    <main className="text-grey-950 min-h-screen  bg-gray-50 px-4 py-16 text-base dark:bg-gray-950 dark:text-gray-50 sm:text-lg">
+      <div className="mx-auto max-w-2xl">
         <Header />
         <Calculator
           numOfDice={numOfDice}
